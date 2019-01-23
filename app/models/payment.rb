c6aa08bcc0934898e0093a44b639e0b01611d1b7
Cesar Valderrama <valderramago@gmail.com>
2019-01-23 15:58:38 +0000

    validaes presence of headquarter, amount and paid at for payment

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -5,6 +5,8 @@ class Payment < ActiveRecord::Base
     after_create :sum_total_payment
     after_destroy :less_total_payment
     
+    validates_presence_of :headquarter, :amount, :paid_at
+    
     scope :between_dates, -> (started_at, finished_at) { where("paid_at > :started_at	AND paid_at <= :finished_at", {started_at: started_at, finished_at: finished_at} ) }
     scope :paid_after, -> (started_at) { where("paid_at > :started_at", {started_at: started_at}) }
     scope :paid_before_eq, -> (started_at) { where("paid_at <= :started_at", {started_at: started_at}) }
